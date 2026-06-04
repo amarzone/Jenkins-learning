27babfc654a57b18a5148546ad7e2e123694b537
amarzone <36321557+amarzone@users.noreply.github.com>
2026-06-04 04:33:43 +0000

    Add new variables for instance and networking configuration

--- variables.tf
+++ variables.tf
@@ -1,8 +1,37 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  validation {
-    condition     = can(regex("^[a-z]{2}-[a-z]+-[0-9]{1}$", var.aws_region))
-    error_message = "Invalid AWS region format."
-  }
+  type = string
 }
+
+variable "instance_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_pair_name" {
+  type = string
+}
+
+variable "security_group_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "associate_public_ip" {
+  type = bool
+}
+
+variable "environment" {
+  type = string
+}
+``
