7a3f087dce19a450c65a7fa496505e1ba6d51c8b
amarzone <36321557+amarzone@users.noreply.github.com>
2026-06-04 04:29:35 +0000

    Update EC2 outputs to reference aws_instance directly

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,31 +1,31 @@
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = module.ec2_node.main.id
+  value       = aws_instance.main.id
 }
 
 output "public_ip" {
   description = "Public IP address of the instance"
-  value       = module.ec2_node.public_ip
+  value       = aws_instance.main.public_ip
 }
 
 output "private_ip" {
   description = "Private IP address of the instance"
-  value       = module.ec2_node.private_ip
+  value       = aws_instance.main.private_ip
 }
 
 output "instance_arn" {
   description = "ARN of the EC2 instance"
-  value       =  module.ec2_node.arn
+  value       = aws_instance.main.arn
 }
 
 output "instance_state" {
   description = "Current state of the instance"
-  value       = module.ec2_node.instance_state
+  value       = aws_instance.main.instance_state
 }
 
 output "availability_zone" {
   description = "Availability zone of the instance"
-  value       = module.ec2_node.availability_zone
+  value       = aws_instance.main.availability_zone
 }
 
 output "security_group_id" {
@@ -35,10 +35,10 @@ output "security_group_id" {
 
 output "vpc_security_group_ids" {
   description = "List of all security group IDs attached to the instance"
-  value       = module.ec2_node.vpc_security_group_ids
+  value       = aws_instance.main.vpc_security_group_ids
 }
 
 output "ssh_command" {
   description = "SSH command to connect to the instance"
-  value       = "ssh -i /path/to/${var.key_pair_name}.pem ec2-user@${module.ec2_node.public_ip}"
+  value       = "ssh -i /path/to/${var.key_pair_name}.pem ec2-user@${aws_instance.main.public_ip}"
 }
