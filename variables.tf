999c6b1e5415b8811199876c6a618a8bff54892c
amarzone <36321557+amarzone@users.noreply.github.com>
2026-06-04 04:45:44 +0000

    Add AWS access and secret key variables

--- variables.tf
+++ variables.tf
@@ -2,6 +2,15 @@ variable "aws_region" {
   type = string
 }
 
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+
 variable "instance_name" {
   type = string
 }
